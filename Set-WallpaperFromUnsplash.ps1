6375d38fde0bdc4b558d6f06cc9784ee0a8ee748
Brian Russell <bmrussell>
2022-02-07 14:34:56 +0000

    added fit options

--- Set-WallpaperFromUnsplash.ps1
+++ Set-WallpaperFromUnsplash.ps1
@@ -1,10 +1,33 @@
-param([string]$Collections = "437035,3652377,8362253")
+param([string]$Collections = "437035,3652377,8362253", [string]$Style='Fill')
+
+
+# Get the developer access key by registering for a developer account at Unsplash https://unsplash.com/join
+# Supply developer access key when prompted to cache in encrypted file
 
 # Run hidden from Task scheduler with
 # pwsh.exe -WindowStyle Hidden -File "Set-WallpaperFromUnsplash.ps1"
-# Supply developer access key when prompted to cache in encrypted file
 
-Function Set-WallPaper($Image) {
+
+Function Set-WallPaper($Image, [string]$Style='Fit') {
+
+    # Set the style of how the wallpaper should be fitted to the desktop resolution
+    $WallpaperStyle = Switch ($Style) { 
+        "Fill" {"10"}
+        "Fit" {"6"}
+        "Stretch" {"2"}
+        "Tile" {"0"}
+        "Center" {"0"}
+        "Span" {"22"}    
+    }
+
+    If($Style -eq "Tile") {
+        New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+        New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 1 -Force 
+    }
+    Else {
+        New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+        New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 0 -Force
+    }
     if (-not ([System.Management.Automation.PSTypeName]'User32Functions').Type)
     {    
         Add-Type -IgnoreWarnings -TypeDefinition @" 
@@ -23,6 +46,7 @@ Function Set-WallPaper($Image) {
     $winIniFlags = $updateIni -bor $fireChangeEvent
     [User32Functions]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $winIniFlags) | Out-Null
 }
+
 
 $creds = (Get-CredentialFromFile.ps1 -File "$($env:USERPROFILE)/Documents/Unsplash.cr")
 $accessKey = $creds.GetNetworkCredential().password
@@ -45,6 +69,8 @@ Invoke-WebRequest $content.urls.raw -Headers $headers -Body @{ fm = "jpg"; w = "
 
 $sel = $content | Select-Object   @{n = "Name"; e = { $_.user.name } }, @{n = "Location"; e = { $_.location.title } }, @{n = "Description"; e = { $_.description } }
 
-$sel | Format-List | Out-File -FilePath "$($env:TEMP)/unsplash.txt"
+($sel | Format-List | Out-String).Replace('[32;1m', '').Replace('[0m','') | Out-File -FilePath "$($env:TEMP)/unsplash.txt"
 
-Set-WallPaper "$($env:TEMP)/unsplash.jpg"
+Set-WallPaper "$($env:TEMP)/unsplash.jpg" $Style
+
+& bginfo /timer:0
